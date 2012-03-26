95ef7c4ba6e389e006e76b62e36ed6340ed14d60
Marc Hadley <mhadley@mitre.org>
2012-03-26 13:38:07 -0400

    Fix indentation in generated code

--- lib/hqmf2js.rb
+++ lib/hqmf2js.rb
@@ -53,39 +53,33 @@ module HQMF2JS
 
       # Convert the HQMF document included as a fixture into JavaScript
       converter = Generator::JS.new(hqmf_contents)
-      converted_hqmf = "#{converter.js_for_data_criteria}
-                        #{converter.js_for('IPP')}
-                        #{converter.js_for('DENOM')}
-                        #{converter.js_for('NUMER')}
-                        #{converter.js_for('DENEXCEP')}"
+      converted_hqmf = "#{converter.js_for_data_criteria}\n#{converter.js_for('IPP')}\n#{converter.js_for('DENOM')}\n#{converter.js_for('NUMER')}\n#{converter.js_for('DENEXCEP')}"
       
       # Pretty stock map/reduce functions that call out to our converted HQMF code stored in the functions variable
       map = "function map(patient) {
-              if (typeof(IPP)==='function' && IPP(patient)) {
-                emit('ipp', 1);
-                if (typeof(DENOM)==='function' && DENOM(patient)) {
-                  if (typeof(NUMER)==='function' && NUMER(patient)) {
-                    emit('denom', 1);
-                    emit('numer', 1);
-                  } else if (typeof(DENEXCEP)==='function' && DENEXCEP(patient)) {
-                    emit('denexcep', 1);
-                  } else {
-                    emit('denom', 1);
-                    emit('antinum', 1);
-                  }
-                }
-              }
-            };"
+  if (typeof(IPP)==='function' && IPP(patient)) {
+    emit('ipp', 1);
+    if (typeof(DENOM)==='function' && DENOM(patient)) {
+      if (typeof(NUMER)==='function' && NUMER(patient)) {
+        emit('denom', 1);
+        emit('numer', 1);
+      } else if (typeof(DENEXCEP)==='function' && DENEXCEP(patient)) {
+        emit('denexcep', 1);
+      } else {
+        emit('denom', 1);
+        emit('antinum', 1);
+      }
+    }
+  }
+};"
       reduce = "function reduce(bucket, counts) {
-                  var sum = 0;
-                  while(counts.hasNext()){
-                    sum += counts.next();
-                  }
-                  return sum;
-                };"
-      functions = "#{hqmf_utils}
-                   var OidDictionary = #{codes_json};
-                   #{converted_hqmf}"
+  var sum = 0;
+  while(counts.hasNext()){
+    sum += counts.next();
+  }
+  return sum;
+};"
+      functions = "#{hqmf_utils}\nvar OidDictionary = #{codes_json};\n#{converted_hqmf}"
 
       return { :map => map, :reduce => reduce, :functions => functions }
     end
